546b92b4cebf14efaab1c09d01d5db710c096934
Gareth Preston <gareth.preston@propertypal.com>
2016-10-22 16:19:13 +0100

    Split up CSS, JS and add in ipv6 ips

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if(isset($domain)) {
   $domain = strtolower(trim($domain));
 
 	if(function_exists('preg_replace')) {
-		// http://www.phpliveregex.com/p/hwF
+		# http://www.phpliveregex.com/p/hwF
 		# Removes www. or http:// or https://
 		$domain = preg_replace("/(https?:\/\/)?www\.([^\/]+)\/?/", "$2", $domain);
 	}
@@ -49,7 +49,10 @@ if(isset($domain)) {
     foreach($dnsr as $record) {
 
       if($record['type'] === 'A')
-        $json->ipaddress[] = $record['ip'];
+        $json->ipaddressV4[] = $record['ip'];
+
+			if($record['type'] === 'AAAA')
+        $json->ipaddressV6[] = $record['ipv6'];
 
 			if($record['type'] === 'TXT')
         $json->txt[] = $record['txt'];
@@ -64,6 +67,14 @@ if(isset($domain)) {
           'priority' => $record['pri']
         );
     }
+
+		// Sort MX Records
+		function mxPriorityAscSort($item1, $item2)
+		{
+		    if ($item1['priority'] === $item2['priority']) return 0;
+		    return ($item1['priority'] > $item2['priority']) ? 1 : -1;
+		}
+		usort($json->mxrecord, 'mxPriorityAscSort');
 
   }
   else if($type === 'whois') {
@@ -97,30 +108,13 @@ if(isset($domain)) {
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="styles.css" type="text/css">
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
-<style type="text/css">
-
-.DnsTools { word-break: break-word; }
-.DnsTools-form,
-.DnsTools-centerText { text-align:center; }
-.DnsTools pre { min-height: 60px; white-space: pre-wrap; white-space: -moz-pre-wrap; white-space: -pre-wrap; white-space: -o-pre-wrap; }
-.DnsTools .panel-heading span { margin-top: -20px; font-size: 15px; }
-.DnsTools-form form { margin:0 auto; width:400px; }
-.DnsTools-form form label { display:none; }
-.DnsTools-form form .btn { margin-top:1em; }
-.DnsTools-whois pre { min-height: 400px; font: 10px/1.2 "Helvetica Neue",Helvetica,Arial,sans-serif; }
-.DnsTools-preview iframe { width: 100%; min-height: 400px; }
-.DnsTools-userInfo { text-align: center; font-size: 0.9em; padding-top: 15px; }
-.DnsTools-userInfo h2 { text-align: left; }
-.DnsTools-userInfo p span { }
-.DnsTools-contentTitle { padding-bottom: 15px; }
-.DnsTools-contentTitle span { font-weight: normal; font-size: 0.8em;}
-</style>
 </head>
 
 <!--[if lt IE 7]>      <body class="DnsTools lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
@@ -147,7 +141,7 @@ if(isset($domain)) {
 
 	<div class="row">
 		<div class="col-sm-12 DnsTools-userInfo">
-			<p class="DnsTools-userIp"><strong>Your IP Address:</strong> <span><?php echo $userIp; ?></span> <span>(<?php echo gethostbyaddr($userIp); ?>)</span></p>
+			<p class="DnsTools-userIp"><strong>Your IP Address:</strong> <span><?php echo ($userIp === '::1')?'127.0.0.1':$userIp; ?></span> <span>(<?php echo gethostbyaddr($userIp); ?>)</span></p>
 			<p class="DnsTools-userAgent"><strong>Your Useragent:</strong> <span><?php echo $_SERVER['HTTP_USER_AGENT']; ?></span></p>
 			<p class="DnsTools-userViewport"><strong>Your viewport:</strong> <span></span></p>
 		</div>
@@ -155,11 +149,6 @@ if(isset($domain)) {
 
 	<hr>
 
-  <!--
-  <div class="alert alert-danger" role="alert">...</div>
-  <div class="alert alert-info" role="alert">loading...</div>
-	http://mxtoolbox.com/SuperTool.aspx?action=blacklist%3abbc.co.uk
-  -->
   <div class="DnsTools-content hidden">
 
 		<h3 class="DnsTools-contentTitle DnsTools-centerText">Results for: <span></span></h3>
@@ -184,10 +173,27 @@ if(isset($domain)) {
 							</div>
 						</div>
           </div>
-          <div class="col-sm-12 col-lg-6 DnsTools-ipAddress">
+          <div class="col-sm-12 col-lg-6 DnsTools-ipAddress DnsTools-ipAddressV4">
             <div class="panel panel-default">
 							<div class="panel-heading">
-						    <h3 class="panel-title">IP Addresses IPv4</h3>
+						    <h3 class="panel-title">IP Address IPv4</h3>
+								<span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-up"></i></span>
+						  </div>
+							<div class="table-responsive">
+								<table class="table table-bordered table-striped">
+									<tbody>
+										<tr>
+											<td></td>
+										</tr>
+									</tbody>
+							  </table>
+							</div>
+						</div>
+          </div>
+					<div class="col-sm-12 col-lg-6 DnsTools-ipAddress DnsTools-ipAddressV6">
+            <div class="panel panel-default">
+							<div class="panel-heading">
+						    <h3 class="panel-title">IP Address IPv6</h3>
 								<span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-up"></i></span>
 						  </div>
 							<div class="table-responsive">
@@ -298,134 +304,7 @@ if(isset($domain)) {
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script type="text/javascript">
-$(document).ready(function() {
+<script type="text/javascript" src="scripts.js"></script>
 
-	function setViewport() {
-		$('.DnsTools-userViewport span').html($(window).width() + 'x' + $(window).height());
-	}
-	setViewport();
-	$(window).resize(setViewport);
-
-	$(document).on('click', '.panel-heading span.clickable', function(e) {
-	  var $this = $(this);
-		if(!$this.hasClass('panel-collapsed')) {
-			var $contentElm = $this.parents('.panel').find('.panel-body');
-			if($contentElm.length == 0)
-				$contentElm = $this.parents('.panel').find('.table-responsive');
-			$contentElm.slideUp();
-			$this.addClass('panel-collapsed');
-			$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-		}
-		else {
-			var $contentElm = $this.parents('.panel').find('.panel-body');
-			if($contentElm.length == 0)
-				$contentElm = $this.parents('.panel').find('.table-responsive');
-			$contentElm.slideDown();
-			$this.removeClass('panel-collapsed');
-			$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-		}
-	});
-
-  $('form').submit(function() {
-
-		var $form = $(this);
-    $('.DnsTools-content, .DnsTools-ipstag').addClass('hidden');
-    $('.input-group', this).removeClass('has-error');
-		$('.DnsTools pre').html('Not Found...');
-
-    var domainName = $("input[name='domain']", this).val().toLowerCase();
-    if(domainName != '') {
-
-			$('.DnsTools-contentTitle span').html(domainName);
-
-      $.get($form.attr('action'), { domain: domainName, type: 'dns' }, function( data ) {
-        console.log(data);
-
-        $('iframe').attr('src', 'http://www.' + data.domain);
-				$('.DnsTools-contentTitle span').html(data.domain);
-
-				if(data.nservers !== undefined) {
-	        $('.DnsTools-nameserver tbody').empty();
-	        for (var i = 0; i < data.nservers.length; i++ ) {
-						$tr = $('<tr />');
-						$tr.append($('<td />').html(data.nservers[i]));
-						$('.DnsTools-nameserver tbody').append($tr);
-					}
-				}
-
-				if(data.ipaddress !== undefined) {
-	        $('.DnsTools-ipAddress tbody').empty();
-	        for (var i = 0; i < data.ipaddress.length; i++ ) {
-						var $tr = $('<tr />');
-						var $link = $('<a />').attr({
-							href: 'http://ip-lookup.net/index.php?ip=' + data.ipaddress[i],
-							target : '_blank'
-						}).html(data.ipaddress[i]);
-						$tr.append($('<td />').append($link));
-						$('.DnsTools-ipAddress tbody').append($tr);
-					}
-				}
-
-				if(data.mxrecord !== undefined) {
-	        $('.DnsTools-mxRecords tbody').empty();
-	        for (var i = 0; i < data.mxrecord.length; i++ ) {
-						var $tr = $('<tr />');
-
-						var $ipLookupLink = $('<a />').attr({
-							href: 'http://ip-lookup.net/index.php?ip=' + data.mxrecord[i].ip,
-							target : '_blank'
-						}).html(data.mxrecord[i].ip);
-
-						var $blacklistLink = $('<a />').attr({
-							href: 'http://mxtoolbox.com/SuperTool.aspx?action=' + encodeURIComponent('blacklist:' + data.mxrecord[i].ip),
-							target : '_blank'
-						}).html('Check');
-
-						$tr.append($('<td />').html(data.mxrecord[i].name));
-						$tr.append($('<td />').addClass('DnsTools-centerText').html(data.mxrecord[i].priority));
-						$tr.append($('<td />').append($ipLookupLink));
-						$tr.append($('<td />').addClass('DnsTools-centerText').append($blacklistLink));
-						$('.DnsTools-mxRecords tbody').append($tr);
-					}
-				}
-
-				if(data.txt !== undefined) {
-	        $('.DnsTools-txtRecords tbody').empty();
-	        for (var i = 0; i < data.txt.length; i++ ) {
-						$tr = $('<tr />');
-						$tr.append($('<td />').html(data.txt[i]));
-						$('.DnsTools-txtRecords tbody').append($tr);
-					}
-				}
-
-      });
-
-      $('.DnsTools-whois pre, .DnsTools-expiryDate pre, .DnsTools-registrar pre, .DnsTools-ipstag pre').empty().html('Please wait....');
-      $.get($form.attr('action'), { domain: domainName, type: 'whois' }, function( data ) {
-        console.log(data);
-
-        $('.DnsTools-whois pre').empty().html(data.output);
-        $('.DnsTools-expiryDate .panel-body').empty().html(new Date(Date.parse(data.expiryDate)).toDateString());
-        $('.DnsTools-registrar .panel-body').empty().html(data.registrar);
-
-				$('.DnsTools-ipstag').addClass('hidden');
-        $('.DnsTools-ipstag .panel-body').empty();
-        if(data.ipstag !== undefined) {
-					$('.DnsTools-ipstag').removeClass('hidden');
-          $('.DnsTools-ipstag  .panel-body').empty().html(data.ipstag);
-				}
-
-      });
-
-      $('.DnsTools-content').removeClass('hidden');
-    }
-    else {
-      $('.input-group', this).addClass('has-error');
-    }
-    return false;
-  });
-});
-</script>
 </body>
 </html>
---
+++ scripts.js
@@ -0,0 +1,149 @@
+
+$(document).ready(function() {
+
+	function setViewport() {
+		$('.DnsTools-userViewport span').html($(window).width() + 'x' + $(window).height());
+	}
+	setViewport();
+	$(window).resize(setViewport);
+
+	$(document).on('click', '.panel-heading span.clickable', function(e) {
+	  var $this = $(this);
+		if(!$this.hasClass('panel-collapsed')) {
+			var $contentElm = $this.parents('.panel').find('.panel-body');
+			if($contentElm.length == 0)
+				$contentElm = $this.parents('.panel').find('.table-responsive');
+			$contentElm.slideUp();
+			$this.addClass('panel-collapsed');
+			$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+		}
+		else {
+			var $contentElm = $this.parents('.panel').find('.panel-body');
+			if($contentElm.length == 0)
+				$contentElm = $this.parents('.panel').find('.table-responsive');
+			$contentElm.slideDown();
+			$this.removeClass('panel-collapsed');
+			$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+		}
+	});
+
+  $('form').submit(function() {
+
+		var $form = $(this);
+    $('.DnsTools-content, .DnsTools-ipstag').addClass('hidden');
+    $('.input-group', this).removeClass('has-error');
+		$('.DnsTools pre').html('Not Found...');
+
+    var domainName = $("input[name='domain']", this).val().toLowerCase();
+    if(domainName != '') {
+
+			$('.DnsTools-contentTitle span').html(domainName);
+
+      $.get($form.attr('action'), { domain: domainName, type: 'dns' }, function( data ) {
+        console.log(data);
+
+        $('iframe').attr('src', 'http://www.' + data.domain);
+				$('.DnsTools-contentTitle span').html(data.domain);
+
+				if(data.nservers !== undefined) {
+	        $('.DnsTools-nameserver tbody').empty();
+	        for (var i = 0; i < data.nservers.length; i++ ) {
+						$tr = $('<tr />');
+						$tr.append($('<td />').html(data.nservers[i]));
+						$('.DnsTools-nameserver tbody').append($tr);
+					}
+				}
+
+				if(data.ipaddressV4 !== undefined) {
+					$('.DnsTools-ipAddressV4').removeClass('hidden');
+					$('.DnsTools-ipAddressV4 tbody').empty();
+	        for (var i = 0; i < data.ipaddressV4.length; i++ ) {
+						var $tr = $('<tr />');
+						var $link = $('<a />').attr({
+							href: 'http://ip-lookup.net/index.php?ip=' + data.ipaddressV4[i],
+							target : '_blank'
+						}).html(data.ipaddressV4[i]);
+						$tr.append($('<td />').append($link));
+						$('.DnsTools-ipAddressV4 tbody').append($tr);
+					}
+				}
+				else {
+					$('.DnsTools-ipAddressV4').addClass('hidden');
+				}
+
+				if(data.ipaddressV6 !== undefined) {
+					$('.DnsTools-ipAddressV6').removeClass('hidden');
+					$('.DnsTools-ipAddressV6 tbody').empty();
+	        for (var i = 0; i < data.ipaddressV6.length; i++ ) {
+						var $tr = $('<tr />');
+						var $link = $('<a />').attr({
+							href: 'http://ip-lookup.net/index.php?ip=' + data.ipaddressV6[i],
+							target : '_blank'
+						}).html(data.ipaddressV6[i]);
+						$tr.append($('<td />').append($link));
+						$('.DnsTools-ipAddressV6 tbody').append($tr);
+					}
+				}
+				else {
+					$('.DnsTools-ipAddressV6').addClass('hidden');
+				}
+
+				if(data.mxrecord !== undefined) {
+	        $('.DnsTools-mxRecords tbody').empty();
+	        for (var i = 0; i < data.mxrecord.length; i++ ) {
+						var $tr = $('<tr />');
+
+						var $ipLookupLink = $('<a />').attr({
+							href: 'http://ip-lookup.net/index.php?ip=' + data.mxrecord[i].ip,
+							target : '_blank'
+						}).html(data.mxrecord[i].ip);
+
+						var $blacklistLink = $('<a />').attr({
+							href: 'http://mxtoolbox.com/SuperTool.aspx?action=' + encodeURIComponent('blacklist:' + data.mxrecord[i].ip),
+							target : '_blank'
+						}).html('Check');
+
+						$tr.append($('<td />').html(data.mxrecord[i].name));
+						$tr.append($('<td />').addClass('DnsTools-centerText').html(data.mxrecord[i].priority));
+						$tr.append($('<td />').append($ipLookupLink));
+						$tr.append($('<td />').addClass('DnsTools-centerText').append($blacklistLink));
+						$('.DnsTools-mxRecords tbody').append($tr);
+					}
+				}
+
+				if(data.txt !== undefined) {
+	        $('.DnsTools-txtRecords tbody').empty();
+	        for (var i = 0; i < data.txt.length; i++ ) {
+						$tr = $('<tr />');
+						$tr.append($('<td />').html(data.txt[i]));
+						$('.DnsTools-txtRecords tbody').append($tr);
+					}
+				}
+
+      });
+
+      $('.DnsTools-whois pre, .DnsTools-expiryDate pre, .DnsTools-registrar pre, .DnsTools-ipstag pre').empty().html('Please wait....');
+      $.get($form.attr('action'), { domain: domainName, type: 'whois' }, function( data ) {
+        console.log(data);
+
+        $('.DnsTools-whois pre').empty().html(data.output);
+        $('.DnsTools-expiryDate .panel-body').empty().html(new Date(Date.parse(data.expiryDate)).toDateString());
+        $('.DnsTools-registrar .panel-body').empty().html(data.registrar);
+
+				$('.DnsTools-ipstag').addClass('hidden');
+        $('.DnsTools-ipstag .panel-body').empty();
+        if(data.ipstag !== undefined) {
+					$('.DnsTools-ipstag').removeClass('hidden');
+          $('.DnsTools-ipstag  .panel-body').empty().html(data.ipstag);
+				}
+
+      });
+
+      $('.DnsTools-content').removeClass('hidden');
+    }
+    else {
+      $('.input-group', this).addClass('has-error');
+    }
+    return false;
+  });
+});
