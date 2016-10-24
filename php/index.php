b94afcb94ec4d6a70b8ff5eda1274a2ca12f98c4
Gareth Preston <gareth.preston@propertypal.com>
2016-10-24 20:52:38 +0100

    Split the bootstrap css with the project css

--- php/index.php
+++ php/index.php
@@ -7,6 +7,7 @@ include_once('main.php');
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="stylesheet" href="/dist/css/bootstrap.min.css" type="text/css">
 <link rel="stylesheet" href="/dist/css/main.min.css" type="text/css">
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
